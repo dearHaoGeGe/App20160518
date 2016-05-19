953c92641e7077b6d25c91179b4bbc59c937b5af
dearHaoGeGe <862157904@qq.com>
2016-05-19 23:42:07 +0800

    滑动Tab完成

---
+++ app/src/main/java/com/my/app20160518/BusinessCirclesFragment.java
@@ -0,0 +1,44 @@
+package com.my.app20160518;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 工商Fragment
+ *
+ * Created by Administrator on 2016/5/19.
+ */
+public class BusinessCirclesFragment extends Fragment {
+
+    public static BusinessCirclesFragment newInstance() {
+        BusinessCirclesFragment fragment = new BusinessCirclesFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.business_circles_fragment,container,false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
--- app/src/main/java/com/my/app20160518/StaffDetailsFragment.java
+++ app/src/main/java/com/my/app20160518/StaffDetailsFragment.java
@@ -1,18 +1,38 @@
 package com.my.app20160518;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.my.app20160518.view.PagerSlidingTabStrip;
+
 /**
  * 案件人员详情
  *
- * Created by Administrator on 2016/5/19.
+ * Created by YJH on 2016/5/19.
  */
 public class StaffDetailsFragment extends Fragment {
+
+    private final Handler handler = new Handler();
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+    private MyPagerAdapter adapter;
+
+    private Drawable oldBackground = null;
+    private int currentColor = 0xFF3F9FE0;    //刚进界面的初始颜色
 
     public static StaffDetailsFragment newInstance() {
         StaffDetailsFragment fragment = new StaffDetailsFragment();
@@ -38,5 +58,139 @@ public class StaffDetailsFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        init();
+    }
+
+    private void init(){
+        tabs = (PagerSlidingTabStrip)getActivity().findViewById(R.id.tabs_StaffDetailsFragment);
+        pager = (ViewPager)getActivity(). findViewById(R.id.pager_StaffDetailsFragment);
+        adapter = new MyPagerAdapter(getActivity().getSupportFragmentManager());
+
+        pager.setAdapter(adapter);
+
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        pager.setPageMargin(pageMargin);
+
+        tabs.setViewPager(pager);
+
+        changeColor(currentColor);
+    }
+
+    private void changeColor(int newColor) {
+
+        tabs.setIndicatorColor(newColor);
+
+        // change ActionBar color just if an ActionBar is available
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+            Drawable colorDrawable = new ColorDrawable(newColor);
+            Drawable bottomDrawable = getResources().getDrawable(R.drawable.actionbar_bottom);
+            LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
+
+            if (oldBackground == null) {
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    ld.setCallback(drawableCallback);
+                } else {
+                    //getActionBar().setBackgroundDrawable(ld);
+                }
+
+            } else {
+
+                TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
+
+                // workaround for broken ActionBarContainer drawable handling on
+                // pre-API 17 builds
+                // https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    td.setCallback(drawableCallback);
+                } else {
+                    //getActionBar().setBackgroundDrawable(td);
+                }
+
+                td.startTransition(200);
+
+            }
+
+            oldBackground = ld;
+
+            // http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
+            //getActionBar().setDisplayShowTitleEnabled(false);
+            //getActionBar().setDisplayShowTitleEnabled(true);
+
+        }
+
+        currentColor = newColor;
+
+    }
+
+    private Drawable.Callback drawableCallback = new Drawable.Callback() {
+        @Override
+        public void invalidateDrawable(Drawable who) {
+            //getActionBar().setBackgroundDrawable(who);
+        }
+
+        @Override
+        public void scheduleDrawable(Drawable who, Runnable what, long when) {
+            handler.postAtTime(what, when);
+        }
+
+        @Override
+        public void unscheduleDrawable(Drawable who, Runnable what) {
+            handler.removeCallbacks(what);
+        }
+    };
+
+    /**
+     * ViewPager适配器
+     */
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        //此处是添加card的标题
+        private final String[] TITLES = {"我的音乐", "网络音乐","都是音乐","呵呵呵呵","测试数据","说四个字","颠覆世界"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return BankFragment.newInstance();
+
+                case 1:
+                    return PoliceFragment.newInstance();
+
+                case 2:
+                    return BusinessCirclesFragment.newInstance();
+
+                case 3:
+                    return BankFragment.newInstance();
+
+                case 4:
+                    return PoliceFragment.newInstance();
+
+                case 5:
+                    return BusinessCirclesFragment.newInstance();
+
+                case 6:
+                    return BankFragment.newInstance();
+            }
+            return null;
+        }
+
     }
 }
