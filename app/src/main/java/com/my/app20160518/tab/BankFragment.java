524b9ad5c9ebe43646ed7d0bc4001389c1515954
dearHaoGeGe <862157904@qq.com>
2016-05-24 17:52:01 +0800

    下班布局

--- app/src/main/java/com/my/app20160518/tab/BankFragment.java
+++ app/src/main/java/com/my/app20160518/tab/BankFragment.java
@@ -2,7 +2,6 @@ package com.my.app20160518.tab;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,19 +27,18 @@ public class BankFragment extends Fragment{
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.bank_fragment,container,false);
         return view;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
 }
